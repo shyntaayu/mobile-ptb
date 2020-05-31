91acd5969811694bdf5d2e6edec895409e91d80a
shyntaayu <shynta.add@gmail.com>
2020-05-31 16:09:47 +0700

    + splash screen done

---
+++ app/src/main/java/com/example/ptb/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package com.example.ptb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private int SPLASH_TIME_OUT = 3000; // 1 sec
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
