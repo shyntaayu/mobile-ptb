a2a5f776756eda33e1141f179f0c2e63baf4bfe2
shyntaayu <shynta.add@gmail.com>
2020-06-17 16:42:33 +0700

    + ketinggalan

--- app/src/main/java/com/example/ptb/MainActivity.java
+++ app/src/main/java/com/example/ptb/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.ptb;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     boolean doubleBackToExitPressedOnce = false;
